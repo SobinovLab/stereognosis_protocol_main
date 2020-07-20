22b7e4820e356641bd8309253f1f0d80ca55f93f
Anton Sobinov <an.sobinov@gmail.com>
2020-07-20 17:46:17 -0500

    setup for experiments

--- ProtocolApp/Protocol/src/ProtocolParameters.cpp
+++ ProtocolApp/Protocol/src/ProtocolParameters.cpp
@@ -40,20 +40,20 @@ void ProtocolParameters::init()
 	speed = 2;        // proportional level 1-10 (1 - 700 RPM)
 	position = 115;     // 1 to 240 mm -> proportional cycles ((-1) to (-105000) CNTs)
 
-	cs_ip1 = "localhost";
+	cs_ip1 = "localhost";  // 205.208.63.128
 	cs_port1 = 63874;
-	cs_ip2 = "localhost";
-	cs_port2 = 63875;
-	cs_framerate = 100;
+	cs_ip2 = "205.208.87.188";
+	cs_port2 = 63874;
+	cs_framerate = 50;
 	cs_recordingPeriod = 25;
-	cs_refSerial = 20050771;
+	cs_refSerial = 19335177;
 
-	tstEnMotors = false;
-	tstEnReward = false;
-	tstEnLightSensors = false;
-	//tstEnMotors = true;
-	//tstEnReward = true;
-	//tstEnLightSensors = true;
+	//tstEnMotors = false;
+	//tstEnReward = false;
+	//tstEnLightSensors = false;
+	tstEnMotors = true;
+	tstEnReward = true;
+	tstEnLightSensors = true;
 
 	tstEnCameras = true;
 	//tstEnCameras = false;
